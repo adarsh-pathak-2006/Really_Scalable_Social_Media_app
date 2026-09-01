546d2ce212d2085058d996ba97eb6867b0594136
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-01 23:53:09 +0530

    going to add views in the core app__follower following logic

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Follow
+from authentication.models import Profile
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class FollowAPI(APIView):
+    def post(self, request, pk):
+        
