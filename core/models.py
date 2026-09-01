ea4cfe3dcfe5182553e3f351ab86baabb28a2f70
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-01 18:49:55 +0530

    imporved follower followwing model

--- core/models.py
+++ core/models.py
@@ -2,9 +2,12 @@ from django.db import models
 from authentication.models import Profile
 
 class Follower(models.Model):
-    user=models.ForeignKey(Profile, on_delete=models.CASCADE)
-    follower=models.ManyToManyField(Profile, related_name='follower_of')
+    follower=models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='following_relationship')
+    following=models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='follower_of')
     created_on=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints=[models.UniqueConstraint(fields=['follower', 'following'], name='unique_follower_following')]
 
     def __str__(self):
         return self.user.name
