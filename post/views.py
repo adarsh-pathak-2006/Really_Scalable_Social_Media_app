64a564dcab38b3921e1c00bb21d6a906c7076f2d
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-01 19:33:21 +0530

    added views in the post app too...also add different endpoints for specific data

--- post/views.py
+++ post/views.py
@@ -3,8 +3,6 @@ from .models import Post, Reel
 from authentication.models import Profile
 from .serializer import PostSerializer, ReelSerializer
 from rest_framework.generics import ListAPIView, ListCreateAPIView
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from social_media.pagination import GeneralReelAndPostPagination
 from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
@@ -29,13 +27,23 @@ class ReelFeedAPI(ListAPIView):
 
 class MyPostAPI(ListCreateAPIView):
     serializer_class=PostSerializer
-    queryset=Post.objects.select_related('user').all()
     pagination_class=GeneralReelAndPostPagination
+
+    def get_queryset(self):
+        return Post.objects.select_related('user').filter(user=self.request.user)
 
     def perform_create(self, serializer):
         profile_data=Profile.objects.select_related('user').all()
         serializer.save(user=profile_data)
 
-    @method_decorator(cache_page(60 * 5))
-    def get(self, request, *args, **kwargs):
-        return super().get(request, *args, **kwargs)
+
+class MyReelAPI(ListCreateAPIView):
+    serializer_class=ReelSerializer
+    pagination_class=GeneralReelAndPostPagination
+
+    def get_queryset(self):
+        return Reel.objects.select_related('user').filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        profile_data=Profile.objects.select_related('user').all()
+        serializer.save(user=profile_data)
